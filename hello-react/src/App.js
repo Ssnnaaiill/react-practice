99ca39983111afd519bbf91ce7c8a1ab1396cda5
Ssnnaaiill <newdk3025@gmail.com>
2019-09-10 00:52:21 +0900

    initial commit and first step of react

---
+++ hello-react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="newApp">
+          리액트 공부하기 좋은 날
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
