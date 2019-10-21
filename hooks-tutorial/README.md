1a903e1b1fb15bc2072fd4a89e1a0da6633d025c
Ssnnaaiill <newdk3025@gmail.com>
2019-10-21 11:19:54 +0900

    counter component using useState hook

---
+++ hooks-tutorial/README.md
@@ -0,0 +1,10 @@
+# hooks-tutorial
+
+## 개요
+
+Hooks는 class 없이 state를 사용할 수 있도록 하는 기능입니다.
+
+- useState
+  가장 기본적인 Hook으로 함수형 컴포넌트에서도 가변적으로 상태 관리를 할 수 있도록 합니다.
+
+- useEffect
---
+++ hooks-tutorial/src/Counter.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <p>
+        Current counter value is <b>{value}</b>.
+      </p>
+      <button onClick={() => setValue(value - 10)}>(-10)</button>
+      <button onClick={() => setValue(value - 1)}>(-1)</button>
+      <button onClick={() => setValue(value + 1)}>(+1)</button>
+      <button onClick={() => setValue(value + 10)}>(+10)</button>
+    </div>
+  );
+};
+
+export default Counter;
